106062603a08db9335e95d7b218abdf7f5292509
Biswajit Bala <Biswajit.Bala@ga.gov.au>
2016-06-08 11:33:32 +1000

    Corrected mosaic script

--- datacube/api/workflow/run_mosaic.sh
+++ datacube/api/workflow/run_mosaic.sh
@@ -13,6 +13,10 @@ for i in $(ls if*);do gdalbuildvrt -input_file_list $i $i.vrt;done
 #step 2  Now run translate for all 9 files
 for i in $(ls if*.vrt); do qsub -v fl=$i run_gdal_translate.pbs;done;
 # combine again to nine files to single mosaic once earlier jobs finished ie step 1 and 2 finished
-ls if*.tif > input_file.txt
-gdalbuildvrt -input_file_list input_file.txt input_file.vrt
-qsub -v fl=input_file.vrt run_gdal_translate.pbs
+ls ifr*.tif > input_file_red.txt
+ls ifg*.tif > input_file_green.txt
+ls ifb*.tif > input_file_blue.txt
+gdalbuildvrt -input_file_list input_file_red.txt input_file_red.vrt
+gdalbuildvrt -input_file_list input_file_green.txt input_file_green.vrt
+gdalbuildvrt -input_file_list input_file_blue.txt input_file_blue.vrt
+for i in $(ls input_file_*.vrt); do qsub -v fl=$i run_gdal_translate.pbs ; done;
